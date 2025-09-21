8611c4403f9a0ec7451a1a92b720e8e63edb7586
ArktosJ <julian.soerensen@outlook.dk>
2025-09-21 14:05:12 +0200

    cleaned it up a bit

--- src/generate.py
+++ src/generate.py
@@ -3,13 +3,12 @@ import os
 from blocks import extract_title, markdown_to_html_node
 
 
-def start_copy_from_to(from_dir, to_dir, base_path="/"):
-    path_from_dir = base_path + "/" + from_dir
-    path_to_dir = base_path + "/" + to_dir
-    if os.path.exists(path_to_dir):
-        shutil.rmtree(path_to_dir)
-    os.mkdir(path_to_dir)
-    copy_from_to(path_from_dir, path_to_dir)
+def start_copy_from_to(from_dir, to_dir):
+    if os.path.exists(to_dir):
+        shutil.rmtree(to_dir)
+    os.mkdir(to_dir)
+    copy_from_to(from_dir, to_dir)
+
 
 def copy_from_to(from_dir, to_dir):
     if os.path.exists(from_dir):
@@ -26,32 +25,23 @@ def copy_from_to(from_dir, to_dir):
         raise Exception("No such path excists")
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path="/"):
-    true_content_path = base_path + "/" + dir_path_content
-    true_dest_path = base_path + "/" + dest_dir_path
-    true_template_path = base_path + "/" + template_path
-    recursive_call(true_content_path, true_template_path, true_dest_path, base_path)
-    pass
-
-
-def recursive_call(dir_path_content, template_path, dest_dir_path, base_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     print(f"looking at {dir_path_content}")
     if os.path.exists(dir_path_content):
         obj_in_dir = os.listdir(dir_path_content)
         for obj in obj_in_dir:
             obj_path = dir_path_content + "/" + obj
             if os.path.isfile(obj_path) and obj.endswith('.md'):
-                generate_page(obj_path, template_path, dest_dir_path, base_path)
+                generate_page(obj_path, template_path, dest_dir_path)
             if os.path.isdir(obj_path):
                 new_dir =  dest_dir_path + "/" + obj
                 os.mkdir(new_dir)
-                recursive_call(obj_path, template_path, new_dir)
+                generate_pages_recursive(obj_path, template_path, new_dir)
     else:
         raise Exception("No such path excists")
 
 
-
-def generate_page(from_path, template_path, dest_path, base_path):
+def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f_from:
         from_file = f_from.read()
@@ -59,8 +49,6 @@ def generate_page(from_path, template_path, dest_path, base_path):
         template_file = f_template.read()
     total_html = template_file.replace("{{ Title }}", extract_title(from_file))
     total_html = total_html.replace("{{ Content }}", markdown_to_html_node(from_file).to_html())
-    total_html = total_html.replace('href="/', 'href=' + base_path)
-    total_html = total_html.replace('src="/', 'src=' + base_path)
     dest_list = os.path.dirname(dest_path).split('/')
     if not os.path.exists(os.path.dirname(dest_path)):
         start_path = "/home/arktosj/StaticSiteGenerator/StaticSiteGenerator" 
--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
-import sys
 from generate import start_copy_from_to, generate_pages_recursive
 
 def main():
 
-    base_path = sys.argv[1]
-    base = "/home/arktosj/StaticSiteGenerator/StaticSiteGenerator"
-    start_copy_from_to("static", "docs", base_path)
+    start_copy_from_to("static", "public")
 
-    generate_pages_recursive("content", "template.html", "docs", base_path)
+    generate_pages_recursive("content", "template.html", "public")
     
 
 if __name__ == "__main__":
